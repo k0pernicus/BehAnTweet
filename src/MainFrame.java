9c3c59fb264f04a8be716b2fe7599ad4befcbf6a
WebTogz <antonin.carette@gmail.com>
2014-09-23 12:12:24 +0200

    Ajout d'une classe Main, pour tester l'interface graphique

---
+++ src/MainFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+import View.MainWindow;
+
+public class MainFrame {
+
+	public static void main(String[] args) {
+
+		MainWindow newMW = new MainWindow("Ma window");
+		
+	}
+
+}
