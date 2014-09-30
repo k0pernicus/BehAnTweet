6e79228abfa78349efdf4a0e4b592a413bfcb08b
WebTogz <antonin.carette@gmail.com>
2014-09-30 10:15:50 +0200

    Ajout d'un test concernant la création d'un JFrame - avec WindowBuilder

---
+++ src/View/TestJFrame.java
@@ -0,0 +1,56 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class TestJFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestJFrame frame = new TestJFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestJFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JTextPane textPane = new JTextPane();
+		contentPane.add(textPane, BorderLayout.CENTER);
+		
+		textField = new JTextField();
+		contentPane.add(textField, BorderLayout.NORTH);
+		textField.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		contentPane.add(btnSearch, BorderLayout.EAST);
+	}
+
+}
