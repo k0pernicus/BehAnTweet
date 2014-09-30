6c90b2aeabc9dc1b4bb4061cfc91153eb6b700e7
WebTogz <antonin.carette@gmail.com>
2014-09-30 12:10:49 +0200

    Ajout du controleur à l'interface - controleur concernant le bouton 'Search'

---
+++ src/Controler/SearchActionListener.java
@@ -0,0 +1,27 @@
+package Controler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import Model.Model;
+import View.ResultPanel;
+
+public class SearchActionListener implements ActionListener {
+
+	private Model model;
+
+	public SearchActionListener(Model model) {
+		this.model = model;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton sButton = (JButton) e.getSource();	
+		ResultPanel parent = (ResultPanel) sButton.getParent();
+		model.run(parent.getSearchText());
+		
+	}
+
+}
