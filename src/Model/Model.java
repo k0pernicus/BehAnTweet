8ff7f51eedb8b8c9be174a4c9a6fb5865821b46a
WebTogz <antonin.carette@gmail.com>
2014-10-06 21:02:12 +0200

    Model.java: Modification du Model - ajout d'opérations concernant les fichiers .csv

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,5 +1,10 @@
 package Model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Observable;
 
 import twitter4j.Query;
@@ -10,10 +15,12 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 public class Model extends Observable {
+	
+	private static final String FILE_NAME = "src/resources/tweets.csv";
 
 	private QueryResult result;
 	
-	public void run(String request){
+	public void run(String request) throws IOException{
 		Twitter twitter = TwitterFactory.getSingleton();
 
 		Query query = new Query(request);
@@ -22,7 +29,9 @@ public class Model extends Observable {
 			updateObservers();
 			
 		    for (Status status : result.getTweets()) {
-		        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+		    	String tweet = new String();
+		    	tweet = status.getId() + ";" + status.getUser().getScreenName() + ";" + status.getText();
+		    	this.writeIntoCSVFile(tweet);
 		    }
 		} catch (TwitterException e) {
 			e.printStackTrace();
@@ -37,4 +46,18 @@ public class Model extends Observable {
 		setChanged();
 		notifyObservers();
 	}
+	
+	void writeIntoCSVFile(String tweet) throws IOException{
+		File csvFile = new File(FILE_NAME); 
+		if (!csvFile.exists()) 
+			throw new FileNotFoundException("Le fichier "+csvFile.getAbsolutePath()+" n'existe pas..."); 
+		else{
+			FileOutputStream in_CSVFile = new FileOutputStream((FILE_NAME), true);
+			in_CSVFile.write(tweet.getBytes());
+			in_CSVFile.write("\n".getBytes());
+			in_CSVFile.flush();
+			in_CSVFile.close();
+		}	
+	}
+	
 }
