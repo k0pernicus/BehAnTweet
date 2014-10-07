e1717fa1030cf734cfa24e4514fa62b5f333786b
WebTogz <antonin.carette@gmail.com>
2014-10-07 09:57:18 +0200

    SearchActionListener.java: Ajout de la documentation développeur

--- src/Controler/SearchActionListener.java
+++ src/Controler/SearchActionListener.java
@@ -10,26 +10,36 @@ import javax.swing.JButton;
 import Model.Model;
 import View.ResultPanel;
 
+/**
+ * ActionListener du bouton "Search" dans le main
+ */
 public class SearchActionListener implements ActionListener {
 
+	/**
+	 * Attribut contenant le modèle du projet
+	 */
 	private Model model;
 
+	/**
+	 * Constructeur de l'objet Listener
+	 * @param model Le modèle du projet
+	 */
 	public SearchActionListener(Model model) {
 		this.model = model;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton sButton = (JButton) e.getSource();	
-		ResultPanel parent = (ResultPanel) sButton.getParent();
+		JButton searchButton = (JButton) e.getSource();	
+		ResultPanel sBParent = (ResultPanel) searchButton.getParent();
 		try {
-			model.run(parent.getSearchText());
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			model.run(sBParent.getSearchText());
+		} catch (FileNotFoundException fileNotFound) {
+			//Fichier non trouvé
+			fileNotFound.printStackTrace();
+		} catch (IOException writeError) {
+			//Erreur lors de l'écriture des octets dans le fichier initial
+			writeError.printStackTrace();
 		}	
 	}
 
