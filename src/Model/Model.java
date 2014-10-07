91535030c2eaab246076f05584fbe31bdbb41e29
WebTogz <antonin.carette@gmail.com>
2014-10-07 09:57:34 +0200

    Model.java: Ajout de la documentation développeur

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -14,13 +14,30 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * Classe Modèle, avec utilisation du modèle MVC
+ * @extends Observable
+ */
 public class Model extends Observable {
 	
+	/**
+	 * Attribut contenant la chaîne de caractères contenant le chemin relatif du fichier CSV contenant tous les tweets (non-nettoyés)
+	 */
 	private static final String FILE_NAME = "src/resources/tweets.csv";
 
+	/**
+	 * Attribut contenant le résultat de la recherche
+	 */
 	private QueryResult result;
 	
-	public void run(String request) throws IOException{
+	/**
+	 * Méthode permettant de faire une requête sur Twitter
+	 * @param request La requête à envoyer
+	 * @throws TwitterException Exception si la requête n'a pas être validée / si la réponse n'a pu nous parvenir
+	 * @throws IOException Exception si l'écriture des tweets n'a pu se faire
+	 * @throws FileNotFoundException Exception si le fichier .csv (utilisé pour y écrire les tweets) n'est pas trouvé
+	 */
+	public void run(String request) throws TwitterException, FileNotFoundException, IOException{
 		Twitter twitter = TwitterFactory.getSingleton();
 
 		Query query = new Query(request);
@@ -47,7 +64,13 @@ public class Model extends Observable {
 		notifyObservers();
 	}
 	
-	void writeIntoCSVFile(String tweet) throws IOException{
+	/**
+	 * Méthode permettant d'écrire tous les tweets récupérés dans un fichier .csv
+	 * @param tweet Une chaîne de caractères contenant le tweet
+	 * @throws FileNotFoundException Le fichier .csv n'a pas été trouvé
+	 * @throws IOException L'écriture du tweet dans le fichier n'a pu se faire
+	 */
+	void writeIntoCSVFile(String tweet) throws FileNotFoundException, IOException{
 		File csvFile = new File(FILE_NAME); 
 		if (!csvFile.exists()) 
 			throw new FileNotFoundException("Le fichier "+csvFile.getAbsolutePath()+" n'existe pas..."); 
