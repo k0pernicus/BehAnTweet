0bedcc7d4db924e2c119af74a314f95822b558ba
Verkyndt Alexandre <alexandre.verkyndt@etudiant.univ-lille1.fr>
2014-10-12 19:36:08 +0200

    Méthode cleanCSVFILE
    La méthode est fini, mais je me demande s'il faut aussi supprimer le
    lien à la fin de certaines lignes et aussi, parfois il y a des caractère
    spéciaux entre les @xxxx genre '' @xxxx'' @yyyyyy A VOIR

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -8,6 +8,8 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Observable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -21,19 +23,19 @@ import twitter4j.TwitterFactory;
  * @extends Observable
  */
 public class Model extends Observable {
-	
+
 	/**
 	 * Attribut contenant la chaîne de caractères contenant le chemin relatif du fichier CSV contenant tous les tweets (non-nettoyés)
 	 */
 	private static final String FILE_NAME = "src/resources/tweets.csv";
-	
+
 	private static final String CLEAN_FILE_NAME = "src/resources/tweets_clean.csv";
 
 	/**
 	 * Attribut contenant le résultat de la recherche
 	 */
 	private QueryResult result;
-	
+
 	//MAIN METHOD
 	/**
 	 * Méthode permettant de faire une requête sur Twitter
@@ -43,41 +45,41 @@ public class Model extends Observable {
 	 * @throws FileNotFoundException Exception si le fichier .csv (utilisé pour y écrire les tweets) n'est pas trouvé
 	 */
 	public void run(String request) throws TwitterException, FileNotFoundException, IOException{
-		
+
 		Twitter twitter = TwitterFactory.getSingleton();
-	
+
 		Query query = new Query(request);
 		try {
-				result = twitter.search(query);
-				updateObservers();
-				
-			    for (Status status : result.getTweets()) {
-			    	String tweet = new String();
-			    	tweet = status.getId() + ";" + status.getUser().getScreenName() + ";\"" + status.getText().replace('\"', '\'').replace('\n', ' ')+"\";" + status.getCreatedAt() + ";" + request;
-			    	this.writeIntoCSVFile(tweet);
-			    }
-			} catch (TwitterException e) {
-				e.printStackTrace();
+			result = twitter.search(query);
+			updateObservers();
+
+			for (Status status : result.getTweets()) {
+				String tweet = new String();
+				tweet = status.getId() + ";" + status.getUser().getScreenName() + ";\"" + status.getText().replace('\"', '\'').replace('\n', ' ')+" \";" + status.getCreatedAt() + ";" + request;
+				this.writeIntoCSVFile(FILE_NAME,tweet);
 			}
-			//TEST
-			this.cleanCSVFile();
-			//TEST
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		//TEST
+		this.cleanCSVFile();
+		//TEST
 	}
-	
+
 	//CSV FILE METHOD
-	
+
 	/**
 	 * Méthode permettant d'écrire tous les tweets récupérés dans un fichier .csv
 	 * @param tweet Une chaîne de caractères contenant le tweet
 	 * @throws FileNotFoundException Le fichier .csv n'a pas été trouvé
 	 * @throws IOException L'écriture du tweet dans le fichier n'a pu se faire
 	 */
-	void writeIntoCSVFile(String tweet) throws FileNotFoundException, IOException{
-		File csvFile = new File(FILE_NAME);
+	void writeIntoCSVFile(String file_name, String tweet) throws FileNotFoundException, IOException{
+		File csvFile = new File(file_name);
 		if (!csvFile.exists()) 
 			throw new FileNotFoundException("Le fichier "+csvFile.getAbsolutePath()+" n'existe pas..."); 
 		else{
-			FileOutputStream in_CSVFile = new FileOutputStream((FILE_NAME), true);
+			FileOutputStream in_CSVFile = new FileOutputStream((file_name), true);
 			in_CSVFile.write(tweet.getBytes());
 			in_CSVFile.write("\n".getBytes());
 			in_CSVFile.flush();
@@ -91,47 +93,66 @@ public class Model extends Observable {
 	 */
 	void cleanCSVFile() throws IOException{//exception à gérer dans le main
 		File csvFile = new File(FILE_NAME);
-		File csvFile_clean   = new File(CLEAN_FILE_NAME);
-		
-		if (!csvFile_clean.exists()) 
+
+		if (!csvFile.exists())
 			throw new FileNotFoundException("Le fichier "+csvFile.getAbsolutePath()+" n'existe pas..."); 
 		else
-			if (!csvFile_clean.exists())
-				throw new FileNotFoundException("Le fichier "+csvFile.getAbsolutePath()+" n'existe pas..."); 
-			else
-			{
-				
-				BufferedReader in_CSVFile = new BufferedReader(new FileReader(FILE_NAME));
-				FileOutputStream out_CSVFile = new FileOutputStream(CLEAN_FILE_NAME, true);
-				
-				while (in_CSVFile.readLine() != null) {
-					
+		{
+
+			BufferedReader in_CSVFile = new BufferedReader(new FileReader(FILE_NAME));
+
+			Pattern p;
+			Matcher m;
+			String line;
+			String patternRT     = "[\\p{Digit}]*;[[\\p{Alpha}]|[\\p{Digit}]]*;\"RT[\\p{ASCII}]*";
+			String patternAROBAS_HASHTAG = "[@|#][[^\\s]&&\\p{ASCII}]*\\s"; // need espace autour des hashtags
+			while ((line = in_CSVFile.readLine()) != null) {
+				p = Pattern.compile(patternRT);	
+				m = p.matcher(line);
+
+				if(m.find()){
+					continue;
 				}
-				
-				/* Ce que l'on veut:
-				 * Pattern Matcher sur chaque ligne du tableau .csv
-				 * 	-> '@' (on kill tout le mot d'après)
-				 * 	-> '#' (on kill tout le mot d'après)
-				 *  -> 'RT' (on n'enregistre rien)
-				 */
-				
+				//LIGNE A CONSERVER 
+				p = Pattern.compile(patternAROBAS_HASHTAG);	
+				m = p.matcher(line);
+
+				StringBuffer sb = new StringBuffer();
+				while(m.find())
+					m.appendReplacement(sb, "");
+				m.appendTail(sb);
+				//SB contient la ligne a conserver sans les @
+
+				line = sb.toString();
+
+				this.writeIntoCSVFile(CLEAN_FILE_NAME,line);
+
 			}
+
+			/* Ce que l'on veut:
+			 * Pattern Matcher sur chaque ligne du tableau .csv
+			 * 	-> '@' (on kill tout le mot d'après) FAIT
+			 * 	-> '#' (on kill tout le mot d'après) FAIT
+			 *  -> 'RT' (on n'enregistre rien) FAIT
+			 */
+
+		}
 	}
-	
-	
+
+
 	//COMMUNICATION WITH THE VIEW
-	
+
 	void updateObservers(){
 		setChanged();
 		notifyObservers();
 	}
-	
-	
-	
+
+
+
 	//GETTERS / SETTERS
 	public QueryResult getResult() {
 		return result;
 	}
-	
-	
+
+
 }
