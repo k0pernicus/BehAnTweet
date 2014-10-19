070756734ca941b83721f161f608af5eb4194367
WebTogz <antonin.carette@gmail.com>
2014-10-19 15:00:09 +0200

    Ajout d'un ActionListener sur le bouton permettant de faire varier le nombre de tweets reçus par une requête

---
+++ src/Controler/NbrTweetsActionListener.java
@@ -0,0 +1,24 @@
+package Controler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+
+import Model.Model;
+
+public class NbrTweetsActionListener implements ActionListener {
+	
+	private Model model;
+	
+	public NbrTweetsActionListener(Model model){
+		this.model = model;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JComboBox rstComboBox = (JComboBox) e.getSource();
+		this.model.setNbrTweets(Integer.parseInt((String) rstComboBox.getSelectedItem()));
+	}
+
+}
