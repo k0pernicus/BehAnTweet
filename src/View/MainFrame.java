cff11d5d1e5058a3c350b33a53d0425f6554639e
WebTogz <antonin.carette@gmail.com>
2014-11-25 11:40:27 +0100

    MainFrame.java : Suppression de code inutile

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -32,7 +32,6 @@ public class MainFrame extends JFrame implements Observer{
 		this.model = model;
 		model.addObserver(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setBounds(100, 100, 650, 400);
 		setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		contentPane = new MainPanel(model);
 		setContentPane(contentPane);
