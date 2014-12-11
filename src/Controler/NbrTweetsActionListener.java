a78f9aeb26f0488e554563b5bd98378669ff3143
WebTogz <antonin.carette@gmail.com>
2014-12-11 20:34:17 +0100

    NbrTweetsActionListener.java : Ajout de la documentation développeur

--- src/Controler/NbrTweetsActionListener.java
+++ src/Controler/NbrTweetsActionListener.java
@@ -8,14 +8,21 @@ import javax.swing.JComboBox;
 import Model.Model;
 
 /**
- * 
+ * Classe NbrTweetsActionListener
+ * Classe permettant d'instancier le Listener du nombre de tweets de la GUI
  * @author antonin
- *
  */
 public class NbrTweetsActionListener implements ActionListener {
 	
+	/**
+	 * Modèle du listener
+	 */
 	private Model model;
 	
+	/**
+	 * Constructeur de la classe
+	 * @param model Modèle du listener
+	 */
 	public NbrTweetsActionListener(Model model){
 		this.model = model;
 	}
