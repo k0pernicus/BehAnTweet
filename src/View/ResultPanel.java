2cf9f7989e0d19fa83a0fa2e4b369de306edd487
WebTogz <antonin.carette@gmail.com>
2014-12-11 21:49:50 +0100

    ResultPanel.java : Ajout de la documentation développeur

--- src/View/ResultPanel.java
+++ src/View/ResultPanel.java
@@ -1,7 +1,6 @@
 package View;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -9,31 +8,53 @@ import javax.swing.JPanel;
 
 import Model.Model;
 
+/**
+ * Classe ResultPanel
+ * Panel permettant d'afficher les résultats de la recherche demandée (tweets et classification de ceux-ci) dans l'application
+ * @author antonin
+ */
 public class ResultPanel extends JPanel implements Observer {
 
+	/**
+	 * Panel de tweets - zone où il est permit d'afficher les tweets réceptionnés
+	 */
 	protected TweetsPanel tweets_panel;
 	
+	/**
+	 * Modèle du projet
+	 */
 	protected Model model;
 	
+	/**
+	 * Constructeur de l'objet ResultPanel
+	 * @param model Le modèle du projet
+	 */
 	public ResultPanel(Model model) {
 		super();
 		
+		/*
+		 * Relation Modèle-objet
+		 */
 		this.model = model;
 		this.model.addObserver((Observer) this);
 		
+		/*
+		 * Affichage au centre de l'application
+		 */
 		this.tweets_panel = new TweetsPanel(model);
 		add(this.tweets_panel, BorderLayout.CENTER);
-		
 	}
 	
-	public String[] getTweetList() throws NullPointerException {
+	/**
+	 * Méthode permettant de récupérer la liste des tweets
+	 * @return Un tableau de tweets
+	 */
+	public String[] getTweetList() {
 		return this.tweets_panel.getTweetList();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
 	}
-	
 }
