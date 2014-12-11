7063296039a3b0843cf9400bea095d68d7c3d081
WebTogz <antonin.carette@gmail.com>
2014-12-11 20:47:18 +0100

    ResultPanel.java : Suppression de la partie statistique

--- src/View/ResultPanel.java
+++ src/View/ResultPanel.java
@@ -13,8 +13,6 @@ public class ResultPanel extends JPanel implements Observer {
 
 	protected TweetsPanel tweets_panel;
 	
-	protected StatPanel stat_panel;
-	
 	protected Model model;
 	
 	public ResultPanel(Model model) {
@@ -24,16 +22,8 @@ public class ResultPanel extends JPanel implements Observer {
 		this.model.addObserver((Observer) this);
 		
 		this.tweets_panel = new TweetsPanel(model);
-		add(this.tweets_panel, BorderLayout.WEST);
+		add(this.tweets_panel, BorderLayout.CENTER);
 		
-		this.stat_panel = new StatPanel(model);
-		this.stat_panel.setVisible(false);
-		add(this.stat_panel, BorderLayout.EAST);
-		
-	}
-	
-	public void setVisibilityStatPanel(boolean bool) {
-		this.stat_panel.setVisible(bool);
 	}
 	
 	public String[] getTweetList() throws NullPointerException {
